47074fd0af0e27ec9f49fc06ec0c3e57a47505c9
João Paulo Franco da Cruz <joaofranco.cruz@gmail.com>
2023-10-16 03:13:37 -0300

    Criação da pasta publuc

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://cnit-homolog.herokuapp.com/',
+  baseURL: process.env.API_URL || 'https://cnit-homolog.herokuapp.com/',
 });
 
 export default api;
