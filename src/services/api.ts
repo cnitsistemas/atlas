f27e6c867ca9bac3df5eecedbd204ab93c37a720
João Paulo Franco da Cruz <joaofranco.cruz@gmail.com>
2023-12-03 19:03:44 -0300

    Correção para o endpoint main do projeto

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.API_URL || 'https://cnit-homolog.herokuapp.com/',
+  baseURL: process.env.API_URL || 'https://cnit.herokuapp.com/',
 });
 
 export default api;
